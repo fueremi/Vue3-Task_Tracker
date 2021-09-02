c5f6cd9554b1778cd935b07d5ecf307c95171a36
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-09-02 12:03:32 +0700

    feat(button): give mobility button to change text and background color

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,29 @@
 <template>
-  <h1>{{ text }}</h1>
+  <header>
+    <h1>{{ text }}</h1>
+    <Button text="Add Task" color="#005511" />
+  </header>
 </template>
 
 <script>
+import Button from "@/components/Button";
+
 export default {
   name: "Header",
   props: {
     text: String,
   },
+  components: {
+    Button,
+  },
 };
 </script>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+</style>
