aef748640ad7c8c6ce0d6889eacd8f82a8402a4a
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-09-02 12:33:14 +0700

    feat(tasks): mobility to enhancement tasks

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,14 +1,19 @@
 <template>
   <div v-for="task in tasks" :key="task.id">
-    <h3>{{ task.task }}</h3>
+    <Task :task="task" />
   </div>
 </template>
 
 <script>
+import Task from "@/components/Task";
+
 export default {
   name: "Tasks",
   props: {
     tasks: Array,
+  },
+  components: {
+    Task,
   },
 };
 </script>
